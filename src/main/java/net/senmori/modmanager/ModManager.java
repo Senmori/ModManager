5c052b7d8ad48e1993fdb9a958921766fdff534f
Senmori <thesenmori@gmail.com>
2018-02-22 22:32:17 -0500

    Initial commit

---
+++ src/main/java/net/senmori/modmanager/ModManager.java
@@ -0,0 +1,15 @@
+package net.senmori.modmanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ModManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
